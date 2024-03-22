a56e44eeeabd2d05cd2f19b3721a57927996fe8a
MrXiaoM <coolxiaom95@gmail.com>
2024-03-23 02:57:27 +0800

    render docs with vuepress

---
+++ README.md
@@ -0,0 +1,7 @@
+# overflow-site
+
+[MrXiaoM/Overflow](https://github.com/MrXiaoM/Overflow) 的官方网站与文档。
+
+主站在 `/docs/.vuepress/public/index.html`，其余文档从源代码仓库拉取。
+
+网站将在提交代码到本仓库时，或每天 12:00 (UTC+0) 自动更新。
---
+++ vuepress.config.js
@@ -0,0 +1,62 @@
+import { viteBundler } from "@vuepress/bundler-vite";
+import { defineUserConfig } from "vuepress";
+import { resolve } from "path";
+import { hopeTheme } from "vuepress-theme-hope";
+
+export default defineUserConfig({
+    title: "Overflow",
+    description: "猫猫式 mirai 实现于 Onebot 11",
+    base: "/",
+    lang: "zh-CN",
+    locales: {
+        "/": {
+            lang: "zh-CN",
+        },
+    },
+    markdown: {
+        lineNumbers: true,
+    },
+    theme: hopeTheme({
+        favicon: "/assets/images/favicon-32x32.png",
+        sidebar: "structure",
+        navbar: [
+            {
+                text: "用户手册",
+                link: "/docs",
+            },
+            {
+                text: "开发文档",
+                link: "/docs/dev",
+            },
+            {
+                text: "部署教程",
+                link: "https://wiki.mrxiaom.top/overflow",
+            }
+        ],
+        print: false,
+        pure: true,
+        repo: "MrXiaoM/Overflow",
+        docsDir: "docs",
+        plugins: {
+            activeHeaderLinks: true,
+            nprogress: true,
+        },
+        lastUpdated: true,
+        breadcrumb: true,
+        pageInfo: false,
+        nextLinks: true,
+        prevLinks: true,
+    }),
+    plugins: [
+    ],
+    bundler: viteBundler({
+        viteOptions: {
+            resolve: {
+                alias: {
+                    "@": resolve(__dirname, "docs"),
+                },
+            },
+        },
+        vuePluginOptions: {},
+    }),
+});
